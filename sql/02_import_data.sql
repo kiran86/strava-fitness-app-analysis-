922d60e5beac36c421166020dd461a48d1b42848
Kiran Sankar Das <kiran.sankar.das@gmail.com>
2026-03-13 20:17:45 +0530

    Refactor import statements for consistency in SQL file

--- sql/02_import_data.sql
+++ sql/02_import_data.sql
@@ -15,34 +15,18 @@ TRUNCATE TABLE
     raw_minute_mets,
     raw_heart_rate;
 
-\copy strava.raw_daily_activity
-FROM 'data/raw/dailyActivity_merged.csv'
-WITH (FORMAT csv, HEADER true);
+\copy strava.raw_daily_activity FROM 'data/raw/dailyActivity_merged.csv' WITH (FORMAT csv, HEADER true);
 
-\copy strava.raw_sleep_day
-FROM 'data/raw/sleepDay_merged.csv'
-WITH (FORMAT csv, HEADER true);
+\copy strava.raw_sleep_day FROM 'data/raw/sleepDay_merged.csv' WITH (FORMAT csv, HEADER true);
 
-\copy strava.raw_weight_log
-FROM 'data/raw/weightLogInfo_merged.csv'
-WITH (FORMAT csv, HEADER true);
+\copy strava.raw_weight_log FROM 'data/raw/weightLogInfo_merged.csv' WITH (FORMAT csv, HEADER true);
 
-\copy strava.raw_hourly_steps
-FROM 'data/raw/hourlySteps_merged.csv'
-WITH (FORMAT csv, HEADER true);
+\copy strava.raw_hourly_steps FROM 'data/raw/hourlySteps_merged.csv' WITH (FORMAT csv, HEADER true);
 
-\copy strava.raw_hourly_calories
-FROM 'data/raw/hourlyCalories_merged.csv'
-WITH (FORMAT csv, HEADER true);
+\copy strava.raw_hourly_calories FROM 'data/raw/hourlyCalories_merged.csv' WITH (FORMAT csv, HEADER true);
 
-\copy strava.raw_hourly_intensities
-FROM 'data/raw/hourlyIntensities_merged.csv'
-WITH (FORMAT csv, HEADER true);
+\copy strava.raw_hourly_intensities FROM 'data/raw/hourlyIntensities_merged.csv' WITH (FORMAT csv, HEADER true);
 
-\copy strava.raw_minute_mets
-FROM 'data/raw/minuteMETsNarrow_merged.csv'
-WITH (FORMAT csv, HEADER true);
+\copy strava.raw_minute_mets FROM 'data/raw/minuteMETsNarrow_merged.csv' WITH (FORMAT csv, HEADER true);
 
-\copy strava.raw_heart_rate
-FROM 'data/raw/heartrate_seconds_merged.csv'
-WITH (FORMAT csv, HEADER true);
+\copy strava.raw_heart_rate FROM 'data/raw/heartrate_seconds_merged.csv' WITH (FORMAT csv, HEADER true);
